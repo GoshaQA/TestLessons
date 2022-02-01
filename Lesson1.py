3fae7528b57519a441186d4d571a402671b77020
georgeiremadze <gosha.iremadze.qa@gmail.com>
2022-02-01 09:56:47 +0200

    Test

---
+++ Lesson1.py
@@ -0,0 +1,72 @@
+#Переменные и типы данных
+
+#Числа
+#
+# a = 5
+# b = 5.0
+#
+# print (a)
+#
+# str_1 = "Text"
+# str_2 = 'Text'
+#
+# str_3 = str_1.replace("xt", "t")
+#
+# print(str_3)
+#
+# print(str_3.replace("t", "T"))
+#
+# #Лист List
+#
+# a_list = list()
+# b_list = []
+#
+# print(type(a_list))
+# print(type(b_list))
+#
+# b_list.append(1)
+# b_list.append(2)
+# b_list.append(3)
+# print(b_list)
+#
+# b_list.extend([1, 2, 3])
+# print(b_list)
+#
+# b_list.reverse()
+# print(b_list)
+#
+# b_list.remove(1)
+# print(b_list)
+#
+# b_list.pop(0)
+# print(b_list)
+#
+# #dict
+#
+# a_dict = dict()
+# b_dict = {}
+# c_dict = {"key": 12}
+#
+# #set
+#
+# a_set = set()
+#
+# #bool
+#
+# a_bool = True
+# b_bool = False
+#
+# #tuple
+#
+# a_tuple = tuple ()
+
+
+#task
+d_list = list()
+
+d_list.append(1)
+d_list.append(2)
+print(d_list)
+
+
+
